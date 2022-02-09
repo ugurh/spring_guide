9d9c5c4b752e21accd61a5b24ae9e550ed5168c0
harun ugur <ugur.harun@yandex.com>
2022-02-09 22:51:53 +0300

    Types of bean scopes (singleton, prototype, request, session, application, and websocket.)

--- src/main/java/io/ugurh/spring_guide/SpringGuideApplication.java
+++ src/main/java/io/ugurh/spring_guide/SpringGuideApplication.java
@@ -1,5 +1,7 @@
 package io.ugurh.spring_guide;
 
+import io.ugurh.spring_guide.domain.CollaborativeFilter;
+import io.ugurh.spring_guide.domain.ContentBasedFilter;
 import io.ugurh.spring_guide.domain.RecommenderImplementation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,18 +14,35 @@ public class SpringGuideApplication {
 
     public static void main(String[] args) {
         //ApplicationContext manages the beans and dependencies
-        ApplicationContext appContext = SpringApplication.run(
-                SpringGuideApplication.class, args);
+        ApplicationContext appContext = SpringApplication.run(SpringGuideApplication.class, args);
 
         //use ApplicationContext to find which filter is being used
-        RecommenderImplementation recommender = appContext.getBean(
-                RecommenderImplementation.class);
+        RecommenderImplementation recommender = appContext.getBean(RecommenderImplementation.class);
 
         //call method to get recommendations
         String[] result = recommender.recommendMovies();
 
         //display results
         System.out.println(Arrays.toString(result));
+
+        System.out.println("*******************************");
+        //Retrieve prototype bean from application context thrice
+        ContentBasedFilter cbf1 = appContext.getBean(ContentBasedFilter.class);
+        ContentBasedFilter cbf2 = appContext.getBean(ContentBasedFilter.class);
+        ContentBasedFilter cbf3 = appContext.getBean(ContentBasedFilter.class);
+
+        System.out.println("ContentBasedFilter 1 -> " + cbf1);
+        System.out.println("ContentBasedFilter 2 -> " + cbf2);
+        System.out.println("ContentBasedFilter 3 -> " + cbf3);
+
+        //Retrieve singleton bean from application context thrice
+        CollaborativeFilter cf1 = appContext.getBean(CollaborativeFilter.class);
+        CollaborativeFilter cf2 = appContext.getBean(CollaborativeFilter.class);
+        CollaborativeFilter cf3 = appContext.getBean(CollaborativeFilter.class);
+
+        System.out.println("CollaborativeFilter 1 -> " + cf1);
+        System.out.println("CollaborativeFilter 2 -> " + cf2);
+        System.out.println("CollaborativeFilter 3 -> " + cf3);
     }
 
 }
--- src/main/java/io/ugurh/spring_guide/domain/ContentBasedFilter.java
+++ src/main/java/io/ugurh/spring_guide/domain/ContentBasedFilter.java
@@ -1,11 +1,15 @@
 package io.ugurh.spring_guide.domain;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
 //@Primary
 @Qualifier("contentBasedFilter")
+//@Scope("Prototype") Option 1
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE) //Option 2
 public class ContentBasedFilter implements Filter{
     @Override
     public String[] recommendMovies() {
